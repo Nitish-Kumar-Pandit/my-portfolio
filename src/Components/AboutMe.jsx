aaa2bb7cd7d0f544297811ce61d5283d0b019e51
Nitish Kumar Pandit <nitishkumarpandit312@gmail.com>
2025-01-06 19:36:48 +0530

    done nothing

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -12,8 +12,8 @@ const AboutMe = () => {
           onMouseLeave={() => {
             gsap.to("#cursor", { scale: 1, duration: 0.3, backgroundColor: "white", mixBlendMode: "difference" });
             }} className="about-content font-medium" >
-        My name is <span className='text-green-500'>Nitish Kumar Pandit</span>. I'm aspiring <strong>FULL Stack Developer</strong>, committed to making a positive impact in the professional world.
-        Driven to create seamless and <span className='text-blue-500'>interactive user experiences</span>,  I'm building my skills as a Full-Stack Developer
+        My name is <span className='text-green-500'>Nitish Kumar Pandit</span>. I am aspiring <strong>FULL Stack Developer</strong>, committed to making a positive impact in the professional world.
+        Driven to create seamless and <span className='text-blue-500'>interactive user experiences</span>,  I am building my skills as a Full-Stack Developer
         while pursuing <span className='text-yellow-500'>B. Tech in Artificial Intelligence and Data Science</span>. I believe in the power of collaboration and I am always learning and looking for opportunities to work with other passionate developers.
       </p>
       <div className="location flex justify-between">#Based in Panchkula, Haryana 
